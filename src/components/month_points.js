0cde7e2fad45c38edcbb8a628afb3b8da1be68ce
Imman Vel <immangeek@gmail.com>
2023-03-19 12:50:56 +0000

    Update 6 files

--- src/components/month_points.js
+++ src/components/month_points.js
@@ -14,24 +14,26 @@ function Monthdata() {
   }, []);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Username</th>
-          <th>Files</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Monthdata.map((user) => (
-          <tr key={user.username}>
-            <td>{user.username}</td>
-            <td>{user.files}</td>
-            <td>{user.points}</td>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th>Username</th>
+            <th>Files</th>
+            <th>Points</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {Monthdata.map((user) => (
+            <tr key={user.username}>
+              <td>{user.username}</td>
+              <td>{user.files}</td>
+              <td>{user.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,7 @@ import Monthdata from "../components/month_points";
 
 export default function Home() {
   return (
+    
     <div>
       <Monthdata />
     </div>
